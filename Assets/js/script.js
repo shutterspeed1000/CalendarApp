5d180c063d9e7849f61fd30c93347431bbb266bc
Sean Worrell <spworrell@gmail.com>
2023-07-25 22:13:59 -0400

    Time working, fields added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,15 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// install Day.js to page
+dayjs().format()
+
+//Set hour for color changes
+var currentHour = dayjs().format(`H`)
+
+//display current day on site
+$(`#currentDay`).text(dayjs().format(`dddd, MMMM D`))
+
+
+{/* <div id="hour-9" class="row time-block past"> */}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,4 +29,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+ 
+
+
 });
+
+
