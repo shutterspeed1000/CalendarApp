f17ebc91fb7f54414a53846f115fd5dade3d5dd9
Sean Worrell <spworrell@gmail.com>
2023-07-28 23:08:26 -0400

    override css for header to center text

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,11 +9,9 @@ $(`#currentDay`).text(dayjs().format(`dddd, MMMM D`));
 
 // run color and data settings after main page loads
 
-$(document).ready(function(){
-setColor()
-
-}
-)
+$(document).ready(function () {
+  setColor();
+});
 
 // select all IDs with hour, compare data hour with current hour.  Set class based on comp.  Make loop
 
@@ -21,11 +19,12 @@ function setColor() {
   $(document).ready(function () {
     $('[id="hour"]').each(function () {
       var slotTime = $(this).data();
-  
-  // Get day info from localstorage
 
-      $('div[data-hour="' + slotTime.hour + '"]').find("textarea").text(localStorage.getItem(slotTime.hour))
+      // Get day info from localstorage
 
+      $('div[data-hour="' + slotTime.hour + '"]')
+        .find("textarea")
+        .text(localStorage.getItem(slotTime.hour));
 
       console.log(slotTime.hour);
 
@@ -49,14 +48,10 @@ function setColor() {
   });
 }
 
-
-
 //Save data when button pushed
-$('button').click(function () {
+$("button").click(function () {
   // Get the content of the textarea
-  var dataSlot = $(this).closest('div').attr(`data-hour`)
-  var dataEntry = $(this).closest('div').children('textarea').val()
-localStorage.setItem(dataSlot,dataEntry)
-})
-
-
+  var dataSlot = $(this).closest("div").attr(`data-hour`);
+  var dataEntry = $(this).closest("div").children("textarea").val();
+  localStorage.setItem(dataSlot, dataEntry);
+});
